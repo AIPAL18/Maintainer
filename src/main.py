e5cfc1f0f81578e565e65928f272c47fed5cbe59
Elie RUGGIERO <elie.ruggiero25@gmail.com>
2025-02-17 18:36:07 +0100

    compatibility

--- src/main.py
+++ src/main.py
@@ -2,28 +2,28 @@ import subprocess
 import json
 from core import get_greater_version, download, remove, unzip, rename
 import os
+import requests
 
 try:
-    releases = subprocess.check_output(['gh', 'api', "/repos/MDL-LDV/Bar-Numerique/releases", "--method", "GET"])
-except subprocess.CalledProcessError as err:
+    releases = requests.get("https://api.github.com/repos/MDL-LDV/Bar-Numerique/releases").json()
+except Exception as err:
     print(err)
-
-releases = json.loads(releases)
+    exit(1)
 
 max_r = get_greater_version(releases) 
 zip_link = max_r["zipball_url"]
 
 path = download(zip_link, max_r["tag_name"] + ".zip")[0]
 
-user = os.getenv("USERPROFILE")
-destination = os.path.join(user, "Documents")
-remove(os.path.join(destination, "Bar numérique"))
+# user = os.getenv("USERPROFILE")
+# destination = os.path.join(user, "Documents")
+# remove(os.path.join(destination, "Bar numérique"))
 
-print(destination)
-folder_name = unzip(path, destination)
+# print(destination)
+# folder_name = unzip(path, destination)
 
-remove(path)
+# remove(path)
 
-temp_folder = os.path.join(destination, folder_name)
-print(temp_folder)
-rename(temp_folder, "Bar numérique")
+# temp_folder = os.path.join(destination, folder_name)
+# print(temp_folder)
+# rename(temp_folder, "Bar numérique")
